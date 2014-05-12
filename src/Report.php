4db02ae451aa593df232e964dff70013a6c49e4a
Klaas Poortinga <poortinga.k@gmail.com>
2014-05-12 10:16:12 +0200

    Rewrote tests, added method addAgent()

--- src/Report.php
+++ src/Report.php
@@ -5,13 +5,12 @@
  *
  * This PHP Report Library can be used in combination with a Learning Record Store
  * to retrieve, analyse and display the records.
- *
  * @package Report
  * @author Klaas Poortinga
- * @version 0.1
+ * @version V0.1
  *
  * @todo Develop a settings page
- * @todo Support method chaining
+ * @todo Option to make a TinCan\Agent - for situations where someone would login
  */
 class Report {
 
@@ -21,6 +20,20 @@ class Report {
     * @var string $version
     */
     private $version = '1.0.1';
+
+    /**
+    * Response placeholder, will containt values returned from methods.
+    *
+    * @var empty $response
+    */
+    public $Statistics;
+
+    /**
+    * Response placeholder, will containt values returned from methods.
+    *
+    * @var empty $response
+    */
+    public $Analyse;
 
     /**
     * method __constructor()
@@ -41,9 +54,13 @@ class Report {
     * @return bool 
     */
     public function connectLrs($lrs_endpoint, $lrs_username, $lrs_password){
-        $this->lrs = new TinCan\RemoteLRS($lrs_endpoint, $this->version, $lrs_username, $lrs_password);
-        $this->Statistics = new Statistics($this->lrs);
-        return true;
+        if($lrs_endpoint && $lrs_username && $lrs_password):
+            $this->lrs = new TinCan\RemoteLRS($lrs_endpoint, $this->version, $lrs_username, $lrs_password);
+            $this->Statistics = new Statistics($this->lrs);
+            $this->Analyse = new Analyse($this->lrs);
+            return true;
+        endif; 
+        return false;
     }
 
 }
--- tests/ReportTest.php
+++ tests/ReportTest.php
@@ -2,12 +2,49 @@
 
 class ReportTest extends PHPUnit_Framework_TestCase {
 
+    private $report;
+
+    public function setUp(){
+        $this->report = new Report();
+    }
+
+    public function tearDown(){
+        //Closing down report class.
+        $this->report = null;
+    }
+
     public function testConnectLrs(){
-        // Arrange
-        $report = new Report();
+        // Set up
+        $endpoint = 'http://learninglocker.banetworks.nl/data/xAPI/';
+        $username = '6e4cd6f48be6114142e4f480ba79831aef335f27';
+        $password = 'dd45015eedb66a514a2ce566ab3af8e11f656da7';
+        $auth = 'Basic ' . base64_encode($username . ':' . $password);
+        $version = '1.0.1';
+
+        // Act: Everything here in preperation and then to check if the TinCan Library is still working as expected.
+        $result = $this->report->connectLrs($endpoint,$username,$password);
 
         // Assert
-        $this->assertTrue($report->connectLrs('http://learninglocker.banetworks.nl/data/xAPI/','6e4cd6f48be6114142e4f480ba79831aef335f27','dd45015eedb66a514a2ce566ab3af8e11f656da7'));
+        $this->assertTrue($result);
+        $this->assertEquals($auth, $this->report->lrs->getAuth());
+        $this->assertEquals($version, $this->report->lrs->getVersion());
+        $this->assertEquals($endpoint, $this->report->lrs->getEndpoint());
+    }
+
+    public function testAddAgent(){
+        // Set up
+        $email = 'klaas.poortinga@brightalley.nl';
+        $name = 'Klaas Poortinga';
+
+        // Act
+        $result = $this->report->addAgent($email, $name);
+        $result2 = $this->report-.addAgent($email);
+
+        // Assert
+        $this->assertTrue($result);
+        $this->assertTrue($result2);
+        $this->assertEquals($email, $this->report->agent->getMbox()); //MOOORE
+
     }
 
 }
