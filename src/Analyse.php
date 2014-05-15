3f1d0b2a0ebc3ec19cad30edb0cec04872fbcee9
Klaas Poortinga <poortinga.k@gmail.com>
2014-05-15 10:41:29 +0200

    Rewrote analyse method, using foreach instead of array_reduce since the latter was giving errors with the public LRS

--- src/Analyse.php
+++ src/Analyse.php
@@ -30,30 +30,33 @@ class Analyse extends Report {
     * @todo Save original statement to display who the suggestion is based on.
     */
     public function getSuggestions($amount = NULL){
-        $result = parent::$lrs->queryStatements([]);
+        $result = parent::$lrs->queryStatements(['limit' => 100]);
         $statements = json_decode($result->httpResponse['_content']);
-        $suggestions = array_reduce($statements->statements, function($v, $item){
-            if(!parent::$agent || $item->actor->mbox != parent::$agent->getMbox()):
-                if(!array_key_exists($item->object->id, $v)){
-                    if(isset($item->object->definition->name->{"en-US"})){
-                        $name = $item->object->definition->name->{"en-US"};
-                    }else{
-                        $name = $item->object->id;
-                    }
-                    $v[$item->object->id] = array('count' => 1, 'title' => $name);
-                }else{
-                    $v[$item->object->id]['count'] += 1;
-                }
-            else:
-                // In this case the statement->actor is equal to the set user in Report::$agent
-                // We take this opportunity to unset statements that have been added while the actor has already done them.
-                if(isset($v[$item->object->id])){
-                    unset($v[$item->object->id]);
-                }
-            endif;
 
-            return $v;
-        }, array());
+        $suggestions = array();
+        foreach($statements->statements as $statement){
+            if(isset($statement->actor->mbox)){
+                if(!parent::$agent || $statement->actor->mbox != parent::$agent->getMbox()){
+                    if(!array_key_exists($statement->object->id, $suggestions)){
+                        if(isset($statement->object->definition->name->{"en-US"})){
+                            $name = $statement->object->definition->name->{"en-US"};
+                        }else{
+                            $name = $statement->object->id;
+                        }
+                        $suggestions[$statement->object->id] = array('count' => 1, 'title' => $name);
+                    }else{
+                        $suggestions[$statement->object->id]['count'] += 1;
+                    }
+                }else{
+                    // In this case the statement->actor is equal to the set user in Report::$agent
+                    // In case the object is set already set, while the actor has done the activity, unset it.
+                    if(isset($suggestions[$statement->object->id])){
+                        unset($suggestions[$statement->object->id]);
+                    }
+                }
+            }
+        }
+
         arsort($suggestions);
 
         if($amount):
